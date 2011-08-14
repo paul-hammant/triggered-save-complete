258ecd628c71139c3d98b3eb86536bb3c77b602f
apaes <Thoughtworks@ap6953.gateway.2wire.net>
2011-08-14 00:57:50 -0500

    Script now with the proper saving function. Saving only once.

--- src/chrome/content/edit_page.js
+++ src/chrome/content/edit_page.js
@@ -1,24 +1,39 @@
 /* oncommand event */
 function save_page_setup() {
-	
+	alert("Beginning to save current page");
 	jQuery.noConflict();
 	$ = function(selector,context) { 
 	    return new jQuery.fn.init(selector,context||window._content.document); 
 	};
 	$.fn = $.prototype = jQuery.fn;
 
-    window.alert("Hello1 " + $('title').text());
-
-	function timerCallback() {
+	//Calling only once. If timed loop wanted, use the commented code 
+	performPersistence();
+	/*function timerCallback() {
 		
 		var signal = $('body').attr('signal');
 		
-		if(signal == undefined) {
-			$('body').attr('signal', "true")
+		if(signal != undefined) {
+			//$('body').attr('signal', "true")
 	         window.alert("Hello3!!!");
+	         performPersistence();
 	    }
-	}
+	}*/
+   // setInterval(timerCallback, 10000);
+}
 
-    setInterval(timerCallback, 10000);
-
+function performPersistence(){
+	//Some Mozilla magic...
+	var persist = Cc["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"].
+	createInstance(Ci.nsIWebBrowserPersist);
+	var localPath = Cc["@mozilla.org/file/local;1"].
+	createInstance(Ci.nsILocalFile);
+	//Local directory where things will be stored
+	localPath.initWithPath("/Users/Thoughtworks/git/testresult");
+	var localFile = localPath.clone();
+	//Saving file based on its title
+	localFile.append($('title').text()+".html");
+	//Must be content.document
+	persist.saveDocument(content.document, localFile, localPath, null, 0, 0);
+	alert("Finished saving current page");
 }
